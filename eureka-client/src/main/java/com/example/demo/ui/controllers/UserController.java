cfdf9385e2677a58ede8ad3aeed06a110b2ce06d
Suyeon Kang <sykang@whatap.io>
2023-01-24 17:24:15 +0900

    Change modules

---
+++ eureka-client/src/main/java/com/example/demo/ui/controllers/UserController.java
@@ -0,0 +1,14 @@
+package com.example.demo.ui.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @GetMapping("/status/check")
+    static String status() {
+        return "working";
+    }
+}

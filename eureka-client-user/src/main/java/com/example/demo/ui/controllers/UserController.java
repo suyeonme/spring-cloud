10ca653aaf9c9d23a30d400cadd0c1dc69cf75ca
Suyeon Kang <sykang@whatap.io>
2023-02-05 21:55:40 +0900

    feat(eureka-client-user): Update application.yml

--- eureka-client-user/src/main/java/com/example/demo/ui/controllers/UserController.java
+++ eureka-client-user/src/main/java/com/example/demo/ui/controllers/UserController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping("/status/check")
     static String status() {
-        return "working";
+        return "User Status working";
     }
 }

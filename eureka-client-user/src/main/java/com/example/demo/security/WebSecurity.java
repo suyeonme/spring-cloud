5576994d01b1da3382784d0ba72d82e04f2657bb
Suyeon Kang <sykang@whatap.io>
2023-02-12 18:25:18 +0900

    config

--- eureka-client-user/src/main/java/com/example/demo/security/WebSecurity.java
+++ eureka-client-user/src/main/java/com/example/demo/security/WebSecurity.java
@@ -1,5 +1,7 @@
 package com.example.demo.security;
 
+import org.hibernate.cfg.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,11 +10,16 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import javax.ws.rs.HttpMethod;
-
 @Configuration
 @EnableWebSecurity
 public class WebSecurity {
+    private Environment env;
+
+    @Autowired
+    public WebSecurity(Environment env) {
+        this.env = env;
+    }
+
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
